3106f19344a55382fe66d8e1e9893ce29b041737
Marcin Mierzwa <marcin.mierzwa81@gmail.com>
2024-09-16 22:38:17 +0200

    update

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
 import { MailController } from './mail.controller';
 import { UserModule } from 'src/user/user.module';
-import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   imports: [UserModule],
